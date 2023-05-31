4529872b5782882fd4c08b7030f129778a5ccb0c
Bryan Choong <bryanchoong12@gmail.com>
2023-05-31 19:28:25 +0800

    dockerfile and bug fixed

--- configHandler.py
+++ configHandler.py
@@ -1,11 +1,13 @@
 import configparser
 
+from logger import log
+
 CONFIG_FILE_NAME = "/data/config.ini"
 config = configparser.ConfigParser()
 
 def generateDefaultConfig():
     sections = ['Settings']
-    settings = [['discord_token', '{token}']]
+    settings = [['discord_token', '{token}'],['discord_server_id', '{id}, {id}']]
 
     for section in sections:
         try:
@@ -23,7 +25,10 @@ def loadConfig():
     try:
         config.read(CONFIG_FILE_NAME)
         config['Settings']['discord_token']
+        config['Settings']['discord_server_id']
     except KeyError:
         generateDefaultConfig()
+        log("Config file not found or incomplete, generating default config file.")
+        exit()
 
     return config
